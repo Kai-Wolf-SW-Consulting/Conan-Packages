c0f7ade747bcdac12980bde076f928b57b8ca958
Kai Wolf <mail@kai-wolf.me>
2019-08-16 12:11:34 +0200

    Fix merge conflict in vtk-dicom

--- vtk-dicom/build.py
+++ vtk-dicom/build.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-# Copyright (c) 2019, Kai Wolf - SW Consulting. All rights reserved.
-# For the licensing terms see LICENSE file in the root directory. For the
-# list of contributors see the AUTHORS file in the same directory.
-
-from conan.packager import ConanMultiPackager
-
-if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=["x86_64"])
-    builder.add_common_builds(pure_c=False)
-    builder.run()
-||||||| merged common ancestors
-=======
 # Copyright (c) 2019, Kai Wolf - SW Consulting. All rights reserved.
 # For the licensing terms see LICENSE file in the root directory. For the
 # list of contributors see the AUTHORS file in the same directory.
@@ -33,4 +20,3 @@ if __name__ == "__main__":
 
     builder.items = items
     builder.run()
->>>>>>> 87bf51380d5b5251fd4ea9a47d65211662202c28
